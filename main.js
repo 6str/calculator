95783a5f4e1137fe0312bd2265f1b3d89809a7e4
phi <postapiano@gmail.com>
2023-01-01 23:22:51 +0000

    code finalising

--- main.js
+++ main.js
@@ -4,11 +4,9 @@ const display_output = document.querySelector('.display .output')
 const modal = document.querySelector('#modal')
 const closeModal = document.querySelector('.close-button')
 
-
 let input = ""
-let input2 = "" 
 
-beeper=new AudioContext() // browsers limit the number of concurrent audio contexts, so you better re-use'em
+beeper=new AudioContext()
 
 function beep(vol, freq, duration){
   v=beeper.createOscillator()
@@ -34,10 +32,10 @@ for (let key of keys) {
 // listener: keyboard key pressed
 document.addEventListener("keydown", (event) => {
    
-    if('1234567890.=+-*/%()'.indexOf(event.key) > -1) keyPress(event.key)
+    if('1234567890.=+-*/%()'.includes(event.key)) keyPress(event.key)
     else if(event.key === 'Enter') keyPress('=')
     else if (event.key === 'Backspace') keyPress('backspace')
-    else if (event.key === 'Escape') keyPress('clear')  
+    else if (event.key === 'Escape') keyPress('clear')
 });
 
 // listener close modal button
@@ -45,11 +43,13 @@ closeModal.addEventListener('click', () => {
     modal.close();
 })
 
+// open credits modal
 function openModal() {
     modal.showModal()
     closeModal.focus()
 }
 
+// set output window data. longer output forces smaller fontsize
 function setOutput(data) {
     if(data.length < 14) {
         display_output.style.fontSize = "3.0rem"
@@ -61,6 +61,43 @@ function setOutput(data) {
     display_output.innerHTML = data;
 }
 
+// operators get padding
+function formatValue(value) {
+    if('+*/-'.includes(value)) return pad(value)
+    else return value
+}
+
+// add a space before and after
+function pad(value) {
+    return ' ' + value + ' '
+}
+
+// delete a character from input
+function backspace() {
+    input = input.trim().slice(0, -1).trim()
+    display_input.innerHTML = formatInput(input)
+}
+
+// evaluate the input expression and set output
+function evaluate(input) {
+
+    input = input
+        .replaceAll('%','/100')
+        .replaceAll('#', '-')
+
+    console.log("evaluate:", input)
+
+    try {
+        setOutput(formatOutput(eval(input))) //eval can be evil but safe here
+    } catch (error) {
+        // if javascript can't evaluate input flash error on screen for 1 second
+        beep(30, 120, 25)
+        console.error("error:", error.message)
+        let temp = display_output.innerHTML
+        setTimeout(() => display_output.innerHTML = temp, 1000)
+        display_output.innerHTML = ` <span class="error">error</span> `
+    }
+}
 
 // process button / key presses
 function keyPress(value) {
@@ -69,200 +106,113 @@ function keyPress(value) {
    
     if (value === "clear") {
         input = ""
-        input2 = ""
         display_input.innerHTML = ""
         display_output.innerHTML = "0"
     } else if (value === 'backspace') {
-        input = input.slice(0, -1)
-        input2 = input2.trim().slice(0, -1)
-        // display_input.innerHTML = formatInput(input)
-        display_input.innerHTML = formatInput(input2)
+        backspace()
+        display_input.innerHTML = formatInput(input)
     } else if (value === "=") {
-        try {
-        let result = evaluate(input2)
-        //display_output.innerHTML = formatOutput(result);
-        setOutput(formatOutput(result)) 
-        } catch (error) {
-            beep(30, 120, 25)
-            console.log("error:", error.message)
-            let temp = display_output.innerHTML
-            setTimeout(() => display_output.innerHTML = temp, 1000)
-            display_output.innerHTML = ` <span class="error">error</span> ` //`<span class="error">Error!</span>`;
-        }
+        evaluate(input)
     } else if (value === "brackets") {
         // open or close bracket?
         if(input.lastIndexOf('(') <= input.lastIndexOf(')')) {
             if(!validInput('(')) return
             input += '('
-            input2 += '('
         }
         else {
             if(!validInput(')')) return
-            input += ')'   
-            input2 += ')'
+            input += ')'
         }
     
-        // display_input.innerHTML = formatInput(input);
-        display_input.innerHTML = formatInput(input2);
+        display_input.innerHTML = formatInput(input);
         
     } else if (value === '-') {
         // allow for negative numbers. i.e. a - that follows an operator
         // # will proxy for - where it's for neg num
-
-        let previousChunk = input2.trim().split(' ').pop()
+        let previousChunk = input.trim().split(' ').pop()
         console.log("minus: prevChunk:", previousChunk)
-        if('+-*/('.includes(previousChunk)) value = '#'
+        let previousChar = previousChunk.trim().split(' ').pop().slice(-1)
+
+        if('+-*/('.includes(previousChar)) value = '#'
 
         if(!validInput(value)) return
-        input += value;
-        input2 += formatValue(value);
-        
-        // display_input.innerHTML = formatInput(input);
-        display_input.innerHTML = formatInput(input2);
+        input += formatValue(value);
+        display_input.innerHTML = formatInput(input);
 
     } else {
 
         if(!validInput(value)) return
-        input += value;
-        input2 += formatValue(value);
-        // display_input.innerHTML = formatInput(input);
-        display_input.innerHTML = formatInput(input2);
+        input += formatValue(value);
+        display_input.innerHTML = formatInput(input);
     }
     
     console.log('input:', input)
-    console.log('input2:', input2)
 }
-
-function formatValue(value) {
-
-    if('+*/-'.indexOf(value) > -1) return pad(value)
-    else return value
-    
-}
-
-function pad(value) {
-    return ' ' + value + ' '
-}
-
-function backspace() {
-    input = input.slice(0, -1)
-    input2 = input2.trim().slice(0, -1)
-    display_input.innerHTML = formatInput(input)
-}
-
-
 
 // validate input against rules
 function validInput(value) {
-    //allow neg numbers
-    //disallow consecutive decimal points or more than one in the same chunk
-    // percent can't follow a space or percent or operator, must be after a number
-    // an operator must follow percent
-    // don't modify the orig input with percent, just replace all when evaluated
-    
-    // mostly: operators can only follow a number ) or %
-    // - and follow another operator in which case it should form a neg number or add a +/- key
-    // percent must be last and followed by and operator
-    //operators must follow a number . ) % i.e. not another operator but - can follow and operator
-    //if minus follows an operator it should be next to the num
-    // if neg num operator must be followed by digits or open bracket
-
-    // to do operator or percent can follow a close bracket
-    // automagically close brackets at the end if last one missing?
-    
     console.log("validInput:", value)
 
-    let previousChunk = input2.trim().split(' ').pop()
+    // the previous character and previous chunk of characters are needed to validate new input
+    let previousChunk = input.trim().split(' ').pop()
     console.log("previous chunk:", previousChunk)
     let previousChar = previousChunk.trim().split(' ').pop().slice(-1)
     console.log('previous char:', previousChar)
 
-
     switch (value) {
-        // case '-':
-
-        case '.':   // no more than 1 decimal point per chunk
-            if(previousChunk.includes('.')) return false
-            if(')'.indexOf(previousChar) > -1) return false
+        case '.':   
+            // no more than 1 decimal point per chunk
+            // decimal can't follow a close bracket
+            if(previousChunk.includes('.')) break //return false
+            if(previousChar === ')') break //return false
             return true
-        case '%':   // must be last the end of a number chunk
-        
-            console.log('%')
-            console.log('previousChar', previousChar)
-            if(previousChar === '') return false
+        case '%':   
+            // must be at the end of a chunk of digits or close bracket
+            if(previousChar === '') break   // n.b. .includes will return true for empty string
             if('1234567890.)'.includes(previousChar)) return true
-        
-            // if(previousChunk.includes('%')) return false
-            // let previousChar = previousChunk.trim().split(' ').pop().slice(-1)
-            // console.log('previousChar:', previousChar)
-            // if('+-*/('.includes(previousChar)) return false
-            // return true
             break
         case '(':
-            // open bracket valid only after an operator or first thing
-            console.log("openBracket", previousChunk)
+            // open bracket valid only after an operator, neg num #, or first thing
             if('+-*/#'.includes(previousChar)) return true
             break
         case ')':
-            // close bracket can't follow an operator 
-            // no need to check for open bracket as this not called unless open bracket somewhere before 
-            // if close braket immediately after open bracket cancel open bracket
-            console.log('input2', input2)
-            console.log('previous item:', input2.trim().split(' ').pop())
-            console.log("is ( valid?")
+            // if close bracket immediately after open bracket clear open bracket
+            // close bracket can't follow an operator or neg num #
             if(previousChar === '(') {
                 backspace()
                 return false
             }
-            if('+-*/'.includes(previousChunk)) return false
+            if('+-*/#'.includes(previousChar)) break
             return true
-            break
-        case '#':
-            if('(+-*/'.indexOf(previousChunk) !== -1) return true
+        case '#':   // # is a proxy for a minus for negative number
+            // valid first thing, after open bracket and after an operator
+            if('(+-*/'.includes(previousChar)) return true
             break
         default:
+            // digits can follow anything but a close bracket or %
             if('0123456789'.includes(value)) {
-                console.log("num:", previousChar)
-                console.log(')'.indexOf(previousChar) === -1)
-                if(')'.indexOf(previousChar) === -1 ||
-                    previousChar === ''
-                ) return true
+                if(')%'.includes(previousChar) && previousChar !== '') break
+                return true
             }
+            // operators can't follow an operator or a minus for neg num # or a . on its own
             else if('+-*/') {
-                console.log("operators:", value)
-                console.log("previousChar:", previousChar)
-                if('+-*/#'.indexOf(previousChar) === -1) return true
+                if('+-*/#('.includes(previousChar)) break
+                if(previousChunk === '.') break
+                return true
             }
             else {
                 console.log('unhandled case:', value)
                 beep(60, 80, 25)
-                return false
             }
-            break
     }
 
-
-    console.log(input)
+    console.log('invalid input:', value)
     beep(20, 120, 25)
+    setTimeout(() => beep(20, 80, 25), 40)
     return false;
 }
 
-
-
-function evaluate(input) {
-    //let result = eval(applyPercent(input))    // note eval safe here by can be dangerous
-    let revisedInput = input
-        .replaceAll('%','/100')
-        .replaceAll('#', '-')
-    
-    console.log("evaluate:", revisedInput)
-    return eval(revisedInput)
-}
-
-
-
-
-
+// format the input expression into human readable input HTML
 function formatInput(input) {
     
     let formattedInput = [...input]
@@ -300,49 +250,32 @@ function formatInput(input) {
     return formattedInput.join('')
 }
 
-
-function formatInputOrig(input) {
-    
-    let formattedInput = [...input]
-
-    for(let i=0; i < formattedInput.length; ++i) {
-        if(formattedInput[i] === "*") {
-            formattedInput[i] = ` <span class="operator">x</span> `
-        } else if(formattedInput[i] === "/") {
-            formattedInput[i] = ` <span class="operator">÷</span> `
-        } else if(formattedInput[i] === "+") {
-            formattedInput[i] = ` <span class="operator">+</span> `
-        } else if(formattedInput[i] === "-") {
-            formattedInput[i] = ` <span class="operator">-</span> `
-        } else if(formattedInput[i] === "(") {
-            formattedInput[i] = `<span class="brackets">(</span>`
-        } else if(formattedInput[i] === ")") {
-            formattedInput[i] = `<span class="brackets">)</span>`
-        } else if(formattedInput[i] === "% ") {
-            formattedInput[i] = `<span class="percent">%</span> `
-        }
-    }
-
-    return formattedInput.join('')
-}
-
-
-// insert 1,000s commas
+// format the output number to human readable
 function formatOutput(output) {
     
+    // insert 1,000s commas
     // loop right to left through the integer part of a number to insert commas
-    // edge cases: numbers is infinite, number is negative
+    // edge cases: number is infinite, some numbers expressed with e notation
+    // no commas for decimal and e portion of a number
 
     if(!isFinite(output)) return output
-
+    
     let formattedOutput = output.toString()
-    let decimalPosition = formattedOutput.indexOf('.')
+    let startPos
 
-    let startPos =  decimalPosition > -1 ? decimalPosition -3 : formattedOutput.length -3  //exclude decimal portion
-    let endPos = isNaN(formattedOutput[0]) ? 1 : 0  //chk for neg num
+    // if e and no decimal startPos is e pos -3, else it the decimal pos -3
+    if(!formattedOutput.includes('.') && formattedOutput.includes('e')) {
+        startPos = formattedOutput.indexOf('e') -3
+    } else {
+        let decimalPosition = formattedOutput.indexOf('.')
+        startPos =  decimalPosition > -1 ? decimalPosition -3 : formattedOutput.length -3  
+    }
+
+    //chk for neg num as first char is -
+    let endPos = isNaN(formattedOutput[0]) ? 1 : 0
     
     formattedOutput = [...formattedOutput]
-
+    
     for(let i = startPos; i > endPos; i-= 3) {
         formattedOutput.splice(i, 0, ',')
     }
